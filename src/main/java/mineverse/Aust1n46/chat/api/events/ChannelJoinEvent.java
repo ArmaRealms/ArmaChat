9813bc6eee7a16f4f8e441ae95f60c48b9219f13
ThiagoROX <51332006+SrBedrock@users.noreply.github.com>
2024-09-02 09:26:33 -0300

    Melhoria dos eventos

--- src/main/java/mineverse/Aust1n46/chat/api/events/ChannelJoinEvent.java
+++ src/main/java/mineverse/Aust1n46/chat/api/events/ChannelJoinEvent.java
@@ -1,36 +1,36 @@
 package mineverse.Aust1n46.chat.api.events;
 
 import mineverse.Aust1n46.chat.channel.ChatChannel;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
-import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+import org.bukkit.event.player.PlayerEvent;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * Event called when a player attempts to join a valid channel
  */
-public class ChannelJoinEvent extends Event implements Cancellable {
-    private static final HandlerList handlers = new HandlerList();
-    private final Player player;
+public class ChannelJoinEvent extends PlayerEvent implements Cancellable {
+    private static final HandlerList HANDLER_LIST = new HandlerList();
     private boolean cancelled;
     private ChatChannel channel;
     private String message;
 
     public ChannelJoinEvent(Player player, ChatChannel channel, String message) {
-        this.player = player;
+        super(player, !Bukkit.isPrimaryThread());
         this.channel = channel;
         this.message = message;
         this.cancelled = false;
     }
 
     public static HandlerList getHandlerList() {
-        return handlers;
+        return HANDLER_LIST;
     }
 
     @Override
     public @NotNull HandlerList getHandlers() {
-        return handlers;
+        return HANDLER_LIST;
     }
 
     @Override
@@ -41,10 +41,6 @@ public class ChannelJoinEvent extends Event implements Cancellable {
     @Override
     public void setCancelled(boolean cancel) {
         this.cancelled = cancel;
-    }
-
-    public Player getPlayer() {
-        return this.player;
     }
 
     public ChatChannel getChannel() {
--- src/main/java/mineverse/Aust1n46/chat/api/events/VentureChatEvent.java
+++ src/main/java/mineverse/Aust1n46/chat/api/events/VentureChatEvent.java
@@ -6,6 +6,7 @@ import mineverse.Aust1n46.chat.channel.ChatChannel;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Set;
 
@@ -104,7 +105,7 @@ public class VentureChatEvent extends Event {
     }
 
     @Override
-    public HandlerList getHandlers() {
+    public @NotNull HandlerList getHandlers() {
         return handlers;
     }
 }
