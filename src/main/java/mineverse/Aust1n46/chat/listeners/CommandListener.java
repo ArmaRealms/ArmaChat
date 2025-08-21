be31e104e443e3c98423ccfcff1256898a204a2c
ThiagoROX <thiagokenis@live.com>
2025-08-21 10:30:44 -0300

    Remove Alias feature and related code
    Deleted the Alias class and removed all references to it in MineverseChat and CommandListener. This change eliminates the command alias functionality, likely for simplification or deprecation.

--- src/main/java/mineverse/Aust1n46/chat/listeners/CommandListener.java
+++ src/main/java/mineverse/Aust1n46/chat/listeners/CommandListener.java
@@ -2,7 +2,6 @@ package mineverse.Aust1n46.chat.listeners;
 
 import me.clip.placeholderapi.PlaceholderAPI;
 import mineverse.Aust1n46.chat.MineverseChat;
-import mineverse.Aust1n46.chat.alias.Alias;
 import mineverse.Aust1n46.chat.api.MineverseChatAPI;
 import mineverse.Aust1n46.chat.api.MineverseChatPlayer;
 import mineverse.Aust1n46.chat.channel.ChatChannel;
@@ -11,7 +10,6 @@ import mineverse.Aust1n46.chat.gui.GuiSlot;
 import mineverse.Aust1n46.chat.localization.LocalizedMessage;
 import mineverse.Aust1n46.chat.utilities.Format;
 import mineverse.Aust1n46.chat.versions.VersionHandler;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.configuration.ConfigurationSection;
@@ -25,19 +23,19 @@ import org.bukkit.event.server.ServerCommandEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
 
-import java.io.FileNotFoundException;
+import java.util.Objects;
 
 public class CommandListener implements Listener {
     private final MineverseChat plugin = MineverseChat.getInstance();
 
     @EventHandler
-    public void onPlayerCommandPreprocess(final PlayerCommandPreprocessEvent event) throws FileNotFoundException {
-        if (event.getPlayer() == null) {
-            Bukkit.getConsoleSender().sendMessage(Format.FormatStringAll("&8[&eVentureChat&8]&c - Event.getPlayer() returned null in PlayerCommandPreprocessEvent"));
+    public void onPlayerCommandPreprocess(final PlayerCommandPreprocessEvent event) {
+        final ConfigurationSection cs = plugin.getConfig().getConfigurationSection("commandspy");
+        if (cs == null) {
+            plugin.getLogger().warning("No commandspy configuration found in the configuration file.");
             return;
         }
-        final ConfigurationSection cs = plugin.getConfig().getConfigurationSection("commandspy");
-        final Boolean wec = cs.getBoolean("worldeditcommands", true);
+        final boolean wec = cs.getBoolean("worldeditcommands", true);
         final MineverseChatPlayer mcp = MineverseChatAPI.getOnlineMineverseChatPlayer(event.getPlayer());
         if (!mcp.getPlayer().hasPermission("venturechat.commandspy.override")) {
             for (final MineverseChatPlayer p : MineverseChatAPI.getOnlineMineverseChatPlayers()) {
@@ -61,68 +59,6 @@ public class CommandListener implements Listener {
         if (mcp.getBlockedCommands().contains(blocked[0])) {
             mcp.getPlayer().sendMessage(LocalizedMessage.BLOCKED_COMMAND.toString().replace("{command}", event.getMessage()));
             event.setCancelled(true);
-            return;
-        }
-
-        final String message = event.getMessage();
-
-        if (Database.isEnabled()) {
-            Database.writeVentureChat(mcp.getUUID().toString(), mcp.getName(), "Local", "Command_Component", event.getMessage().replace("'", "''"), "Command");
-        }
-
-        for (final Alias a : Alias.getAliases()) {
-            if (message.toLowerCase().substring(1).split(" ")[0].equals(a.name().toLowerCase())) {
-                for (String s : a.components()) {
-                    if (!mcp.getPlayer().hasPermission(a.permission()) && a.hasPermission()) {
-                        mcp.getPlayer().sendMessage(ChatColor.RED + "You do not have permission for this alias.");
-                        event.setCancelled(true);
-                        return;
-                    }
-                    int num = 1;
-                    if (message.length() < a.name().length() + 2 || a.arguments() == 0)
-                        num = 0;
-                    int arg = 0;
-                    if (message.substring(a.name().length() + 1 + num).length() == 0)
-                        arg = 1;
-                    final String[] args = message.substring(a.name().length() + 1 + num).split(" ");
-                    String send = "";
-                    if (args.length - arg < a.arguments()) {
-                        String keyword = "arguments.";
-                        if (a.arguments() == 1)
-                            keyword = "argument.";
-                        mcp.getPlayer().sendMessage(ChatColor.RED + "Invalid arguments for this alias, enter at least " + a.arguments() + " " + keyword);
-                        event.setCancelled(true);
-                        return;
-                    }
-                    for (int b = 0; b < args.length; b++) {
-                        send += " " + args[b];
-                    }
-                    if (send.length() > 0)
-                        send = send.substring(1);
-                    s = Format.FormatStringAll(s);
-                    if (mcp.getPlayer().hasPermission("venturechat.color.legacy")) {
-                        send = Format.FormatStringLegacyColor(send);
-                    }
-                    if (mcp.getPlayer().hasPermission("venturechat.color")) {
-                        send = Format.FormatStringColor(send);
-                    }
-                    if (mcp.getPlayer().hasPermission("venturechat.format")) {
-                        send = Format.FormatString(send);
-                    }
-                    if (s.startsWith("Command:")) {
-                        mcp.getPlayer().chat(s.substring(9).replace("$", send));
-                        event.setCancelled(true);
-                    }
-                    if (s.startsWith("Message:")) {
-                        mcp.getPlayer().sendMessage(s.substring(9).replace("$", send));
-                        event.setCancelled(true);
-                    }
-                    if (s.startsWith("Broadcast:")) {
-                        Format.broadcastToServer(s.substring(11).replace("$", send));
-                        event.setCancelled(true);
-                    }
-                }
-            }
         }
     }
 
@@ -147,7 +83,7 @@ public class CommandListener implements Listener {
         final MineverseChatPlayer target = MineverseChatAPI.getMineverseChatPlayer(playerName);
         final ItemStack skull = e.getInventory().getItem(0);
         final SkullMeta skullMeta = (SkullMeta) skull.getItemMeta();
-        final ChatChannel channel = ChatChannel.getChannel(ChatColor.stripColor(skullMeta.getLore().get(0)).replace("Channel: ", ""));
+        final ChatChannel channel = ChatChannel.getChannel(ChatColor.stripColor(Objects.requireNonNull(skullMeta.getLore()).getFirst()).replace("Channel: ", ""));
         final int hash = Integer.parseInt(ChatColor.stripColor(skullMeta.getLore().get(1).replace("Hash: ", "")));
         if (VersionHandler.is1_7()) {
             if (item.getType() == Material.BEDROCK) {
